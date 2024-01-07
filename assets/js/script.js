874f2e2963eecf7c718015ce7067565a634b6b71
Roman Detkin <vanderveldekr@gmail.com>
2024-01-07 21:49:54 +0500

    + mobile version

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,47 @@ var swiper = new Swiper(".swiper_tours", {
       prevEl: ".swiper-button-prev",
     },
 });
+
+var swiper = new Swiper(".slider-vedeo", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "3",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+    let links = menu.find('.burger-menu_link');
+    let overlay = menu.find('.burger-menu_overlay');
+    
+    button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+    });
+    
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+    
+    function toggleMenu(){
+    menu.toggleClass('burger-menu_active');
+    
+    if (menu.hasClass('burger-menu_active')) {
+        $('body').css('overflow', 'hidden');
+    } else {
+        $('body').css('overflow', 'visible');
+    }
+    }
+}
+burgerMenu('.burger-menu');
