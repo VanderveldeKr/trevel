1d8f1a510e1407df66077be5f4e182a3bfa7049d
Roman Detkin <vanderveldekr@gmail.com>
2024-01-07 11:26:29 +0500

    tours desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,11 @@ var swiper = new Swiper(".swiper_video", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+});
+var swiper = new Swiper(".swiper_tours", {
+    rewind: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
